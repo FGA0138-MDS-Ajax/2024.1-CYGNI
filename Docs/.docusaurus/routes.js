8f6cebded0d62f8c7c180ae51a2ea5a2e8a5921d
junioramaral22 <junior.amaral.1818@gmail.com>
2024-05-21 00:52:15 -0300

    Ignorar arquivos :Zone.Identifier

---
+++ Docs/.docusaurus/routes.js
@@ -0,0 +1,186 @@
+import React from 'react';
+import ComponentCreator from '@docusaurus/ComponentCreator';
+
+export default [
+  {
+    path: '/2024.1-CYGNI/__docusaurus/debug',
+    component: ComponentCreator('/2024.1-CYGNI/__docusaurus/debug', '76e'),
+    exact: true
+  },
+  {
+    path: '/2024.1-CYGNI/__docusaurus/debug/config',
+    component: ComponentCreator('/2024.1-CYGNI/__docusaurus/debug/config', 'be0'),
+    exact: true
+  },
+  {
+    path: '/2024.1-CYGNI/__docusaurus/debug/content',
+    component: ComponentCreator('/2024.1-CYGNI/__docusaurus/debug/content', 'cbe'),
+    exact: true
+  },
+  {
+    path: '/2024.1-CYGNI/__docusaurus/debug/globalData',
+    component: ComponentCreator('/2024.1-CYGNI/__docusaurus/debug/globalData', '86b'),
+    exact: true
+  },
+  {
+    path: '/2024.1-CYGNI/__docusaurus/debug/metadata',
+    component: ComponentCreator('/2024.1-CYGNI/__docusaurus/debug/metadata', 'f75'),
+    exact: true
+  },
+  {
+    path: '/2024.1-CYGNI/__docusaurus/debug/registry',
+    component: ComponentCreator('/2024.1-CYGNI/__docusaurus/debug/registry', '9f8'),
+    exact: true
+  },
+  {
+    path: '/2024.1-CYGNI/__docusaurus/debug/routes',
+    component: ComponentCreator('/2024.1-CYGNI/__docusaurus/debug/routes', '558'),
+    exact: true
+  },
+  {
+    path: '/2024.1-CYGNI/markdown-page',
+    component: ComponentCreator('/2024.1-CYGNI/markdown-page', '263'),
+    exact: true
+  },
+  {
+    path: '/2024.1-CYGNI/docs',
+    component: ComponentCreator('/2024.1-CYGNI/docs', 'dcc'),
+    routes: [
+      {
+        path: '/2024.1-CYGNI/docs',
+        component: ComponentCreator('/2024.1-CYGNI/docs', 'a67'),
+        routes: [
+          {
+            path: '/2024.1-CYGNI/docs',
+            component: ComponentCreator('/2024.1-CYGNI/docs', 'cbf'),
+            routes: [
+              {
+                path: '/2024.1-CYGNI/docs/atas-reuniao/AtaReuniao01-05',
+                component: ComponentCreator('/2024.1-CYGNI/docs/atas-reuniao/AtaReuniao01-05', 'e62'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/2024.1-CYGNI/docs/atas-reuniao/AtaReunião08-05',
+                component: ComponentCreator('/2024.1-CYGNI/docs/atas-reuniao/AtaReunião08-05', 'c78'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/2024.1-CYGNI/docs/atas-reuniao/AtaReuniao12-05',
+                component: ComponentCreator('/2024.1-CYGNI/docs/atas-reuniao/AtaReuniao12-05', '1e9'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/2024.1-CYGNI/docs/atas-reuniao/AtaReuniao24-04',
+                component: ComponentCreator('/2024.1-CYGNI/docs/atas-reuniao/AtaReuniao24-04', 'b90'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/2024.1-CYGNI/docs/atas-reuniao/AtasReunioesModelo',
+                component: ComponentCreator('/2024.1-CYGNI/docs/atas-reuniao/AtasReunioesModelo', '5ac'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/2024.1-CYGNI/docs/category/atas-de-reunião',
+                component: ComponentCreator('/2024.1-CYGNI/docs/category/atas-de-reunião', 'd46'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/2024.1-CYGNI/docs/category/sobre-o-agis',
+                component: ComponentCreator('/2024.1-CYGNI/docs/category/sobre-o-agis', 'ad6'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/2024.1-CYGNI/docs/category/sobre-o-projeto',
+                component: ComponentCreator('/2024.1-CYGNI/docs/category/sobre-o-projeto', '599'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/2024.1-CYGNI/docs/intro',
+                component: ComponentCreator('/2024.1-CYGNI/docs/intro', 'a00'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/2024.1-CYGNI/docs/sobre-o-agis/equipe',
+                component: ComponentCreator('/2024.1-CYGNI/docs/sobre-o-agis/equipe', '6cb'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/2024.1-CYGNI/docs/sobre-o-agis/o-agis',
+                component: ComponentCreator('/2024.1-CYGNI/docs/sobre-o-agis/o-agis', 'e2e'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/2024.1-CYGNI/docs/sobre-o-projeto/ciclo-de-vida',
+                component: ComponentCreator('/2024.1-CYGNI/docs/sobre-o-projeto/ciclo-de-vida', '250'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/2024.1-CYGNI/docs/sobre-o-projeto/criterios-de-replanejamento',
+                component: ComponentCreator('/2024.1-CYGNI/docs/sobre-o-projeto/criterios-de-replanejamento', 'cf3'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/2024.1-CYGNI/docs/sobre-o-projeto/declaracao-do-escopo-do-projeto',
+                component: ComponentCreator('/2024.1-CYGNI/docs/sobre-o-projeto/declaracao-do-escopo-do-projeto', 'ccf'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/2024.1-CYGNI/docs/sobre-o-projeto/funcoesdelegadas',
+                component: ComponentCreator('/2024.1-CYGNI/docs/sobre-o-projeto/funcoesdelegadas', '949'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/2024.1-CYGNI/docs/sobre-o-projeto/gerenciamento-de-riscos',
+                component: ComponentCreator('/2024.1-CYGNI/docs/sobre-o-projeto/gerenciamento-de-riscos', '2ef'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/2024.1-CYGNI/docs/sobre-o-projeto/matriz-de-comunicação',
+                component: ComponentCreator('/2024.1-CYGNI/docs/sobre-o-projeto/matriz-de-comunicação', '5fb'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/2024.1-CYGNI/docs/sobre-o-projeto/planejamento-de-fases',
+                component: ComponentCreator('/2024.1-CYGNI/docs/sobre-o-projeto/planejamento-de-fases', '620'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/2024.1-CYGNI/docs/sobre-o-projeto/processos-de-desenvolvimento-de-software',
+                component: ComponentCreator('/2024.1-CYGNI/docs/sobre-o-projeto/processos-de-desenvolvimento-de-software', '9cc'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: '/2024.1-CYGNI/',
+    component: ComponentCreator('/2024.1-CYGNI/', '924'),
+    exact: true
+  },
+  {
+    path: '*',
+    component: ComponentCreator('*'),
+  },
+];
