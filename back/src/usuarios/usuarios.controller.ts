478fb2325768ce8de476c0a65ba43dd6d6454ee1
Ian Costa <iancostag@outlook.com>
2024-06-02 22:07:16 -0300

    Feat:Remover Funcionário

--- back/src/usuarios/usuarios.controller.ts
+++ back/src/usuarios/usuarios.controller.ts
@@ -29,6 +29,6 @@ export class UsuariosController {
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.usuariosService.remove(+id);
+    return this.usuariosService.remove(id);
   }
 }
