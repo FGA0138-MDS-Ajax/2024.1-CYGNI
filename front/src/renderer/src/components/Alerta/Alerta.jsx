e773f12bdf568760a5ea537504cb73ab2c590c48
caua08 <cau411@hotmail.com>
2024-06-20 16:21:10 -0300

    refactor: mudanças visuais na tela de login e adição da mensagem de alerta nas condições de login

---
+++ front/src/renderer/src/components/Alerta/Alerta.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Alerta.css";
+
+const Alert = ({ message, type, onClose }) => (
+	<div className={`alert alert-${type}`}>
+		{message}
+		<button className="close-btn" onClick={onClose}>
+			X
+		</button>
+	</div>
+);
+
+export default Alert;
