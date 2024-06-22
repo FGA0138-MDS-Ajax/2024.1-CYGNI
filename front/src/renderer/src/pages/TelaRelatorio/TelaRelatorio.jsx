767e6c031eb11cc4209575d203229e1e1dd9236a
ailujana <anajumesantos11@hotmail.com>
2024-06-22 16:21:03 -0300

    fix: ajustada tabela de relatório mensal

--- front/src/renderer/src/pages/TelaRelatorio/TelaRelatorio.jsx
+++ front/src/renderer/src/pages/TelaRelatorio/TelaRelatorio.jsx
@@ -6,7 +6,7 @@ import MenuLateral from "../../components/MenuLateral/MenuLateral";
 
 
 import "./TelaRelatorio.css";
-import {DataTable} from '../../components/TabelaRel/Tabela'
+import { DataTable } from '../../components/TabelaRelatorio/Tabela'
 
 const TelaRelatorio = () => {
 	const {
@@ -19,87 +19,10 @@ const TelaRelatorio = () => {
 	return (
 		<div className="conteiner-relatorio-mensal">
 			<MenuLateral />
-			<div className="conteiner-conteudo-principal">
-				<div className="filtros">
-					{/* <Campo id="ano" texto="Ano" tipo="text" registro={register} erros={errors} /> */}
-					<MenuSuspenso
-						id="ano"
-						texto="Ano"
-						largura="190px"
-						opcoes={[
-							"2024",
-							"2025",
-							"2026",
-							"2027",
-							"2028",
-							"2029",
-							"2030",
-							"2031",
-							"2032",
-							"2033",
-							"2034",
-							"2035",
-							"2036",
-							"2037",
-							"2038",
-							"2039",
-							"2040",
-							"2041",
-							"2042",
-							"2043",
-							"2044",
-							"2045",
-							"2046",
-							"2047",
-							"2048",
-							"2049",
-							"2050",
-						]}
-					/>
-					<MenuSuspenso
-						id="mes"
-						texto="Mês"
-						largura="190px"
-						opcoes={[
-							"Janeiro",
-							"Fevereiro",
-							"Março",
-							"Abril",
-							"Maio",
-							"Junho",
-							"Julho",
-							"Agosto",
-							"Setembro",
-							"Outubro",
-							"Novembro",
-							"Dezembro",
-						]}
-					/>
-					<MenuSuspenso
-						id="situacao"
-						texto="Situação"
-						largura="190px"
-						opcoes={[
-							"Abono",
-							"LTSP",
-							"Férias",
-							"LTIP",
-							"LTSPF",
-							"Núpcias",
-							"Licença Paternidade",
-							"Dispensa Recompensa",
-							"Restrição Médica",
-							"Recesso Final de Ano",
-							"Atestado de Comparecimento",
-							"Atestado de Acompanhamento",
-							"Outros",
-						]}
-					/>
-					<MenuSuspenso id="ordem" texto="Ordem" largura="190px" opcoes={["A-Z", "Z-A"]} />
-				</div>
-				<div className="conteiner-relatorio">
-					<DataTable/>
-				</div>
+
+			<div className="conteiner-relatorio">
+				<DataTable
+				/>
 			</div>
 		</div>
 	);
